0e3f43d4c512f5409d9ff0a7183ad7161038f062
YAMASAKI Masahide <masahide.y@gmail.com>
2023-12-25 00:41:07 +0900

    fix

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -156,8 +156,9 @@
       <h1>inet-ip.info</h1>
       <p class="lead">
         This website is a web service that allows you to check your current IP address. It is a web service written in the Go language, and
-        its source code is publicly available on GitHub. This service is particularly useful for individuals and developers who need to
-        quickly determine their public IP address for various network tasks, troubleshooting, or development purposes.
+        its source code is publicly available on <a href="https://github.com/inet-ip-info/website/" class="link-underline-primary">GitHub</a
+        >. This service is particularly useful for individuals and developers who need to quickly determine their public IP address for
+        various network tasks, troubleshooting, or development purposes.
       </p>
     </div>
 
